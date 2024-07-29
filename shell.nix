38a8bc25b7d89fb4b5e5c02ec429592e280a05d3
atmatm9182 <atmatm9182@gmail.com>
2024-07-29 22:52:06 +0200

    feat: added kcachegrind to the shell.nix development shell

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@ with import <nixpkgs> {};
 
 llvmPackages_17.stdenv.mkDerivation {
   name = "c-env";
-  nativeBuildInputs = [ clang-tools_17 meson ninja lldb valgrind ];
+  nativeBuildInputs = [ clang-tools_17 meson ninja lldb valgrind kcachegrind ];
   hardeningDisable = [ "fortify" ];
 }
