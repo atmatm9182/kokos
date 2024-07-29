7381bdb319d175da1aec13f42db119d51ccf1fc4
atmatm9182 <atmatm9182@gmail.com>
2024-07-29 22:51:41 +0200

    fix: wrong `kokos_scope_get_var_idx` implementation

--- vm/src/compile.c
+++ vm/src/compile.c
@@ -180,13 +180,18 @@ static bool compile_procedure_def(const kokos_expr_t* expr, kokos_scope_t* scope
 
 static size_t kokos_scope_get_var_idx(const kokos_scope_t* scope, string_view name)
 {
+    if (!scope) {
+        return (size_t)-1;
+    }
+
     kokos_variable_list_t vars = scope->vars;
     for (size_t i = 0; i < vars.len; i++) {
         if (sv_eq(name, vars.items[i])) {
             return i;
         }
     }
-    return (size_t)-1;
+
+    return kokos_scope_get_var_idx(scope->parent, name);
 }
 
 static bool get_special_value(string_view value, uint64_t* out)
@@ -289,7 +294,7 @@ static bool compile_if(const kokos_expr_t* expr, kokos_scope_t* scope)
         DA_ADD(code, INSTR_BRANCH(0));
 
         kokos_scope_t alternative_scope = kokos_scope_empty(scope, kokos_scope_is_top_level(scope));
-        
+
         size_t consequence_ip = code->len - start_ip;
         TRY(kokos_expr_compile(exprs.items[3], &alternative_scope));
 
